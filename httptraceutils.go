e6e4f5e936f2fb611ebd6d78ec7e4f50c927eff6
tcnksm <nsd22843@gmail.com>
2016-08-13 17:26:43 +0900

    Fix log format

--- httptraceutils.go
+++ httptraceutils.go
@@ -6,14 +6,19 @@ import (
 	"log"
 	"net/http/httptrace"
 	"os"
+	"strings"
 )
 
 var defaultLogger = log.New(os.Stderr, "", log.LstdFlags)
 
+// WithClientTrace returns a new context based on the provided parent
+// ctx.
 func WithClientTrace(ctx context.Context) context.Context {
 	return WithClientTraceLogger(ctx, defaultLogger)
 }
 
+// WithClientTrace returns a new context based on the provided parent
+// ctx.
 func WithClientTraceLogger(ctx context.Context, logger *log.Logger) context.Context {
 	return httptrace.WithClientTrace(ctx, newClientTrace(logger))
 }
@@ -21,15 +26,25 @@ func WithClientTraceLogger(ctx context.Context, logger *log.Logger) context.Cont
 func newClientTrace(logger *log.Logger) *httptrace.ClientTrace {
 	return &httptrace.ClientTrace{
 		GetConn: func(hostPort string) {
-			logger.Printf("[GetConn] hostPort: %s", hostPort)
+			logger.Printf("[GetConn] HostPort: %s", hostPort)
 		},
 
 		GotConn: func(info httptrace.GotConnInfo) {
-			logger.Printf("[GotConn] gotConnInfo: %#v", info)
+			logger.Printf("[GotConn] LocalAddr: %s", info.Conn.LocalAddr())
+			logger.Printf("[GotConn] RemoteAddr: %s", info.Conn.RemoteAddr())
+			logger.Printf("[GotConn] Reused: %v", info.Reused)
+			logger.Printf("[GotConn] WasIdle: %#v", info.WasIdle)
+			if info.WasIdle {
+				logger.Printf("[GotConn] IdleTime: %#v", info.IdleTime)
+			}
 		},
 
 		PutIdleConn: func(err error) {
-			logger.Printf("[PutIdeConn] err: %s", err)
+			if err != nil {
+				logger.Printf("[PutIdeConn] Error: %s", err)
+				return
+			}
+			logger.Printf("[PutIdeConn] Error: nil")
 		},
 
 		GotFirstResponseByte: func() {
@@ -41,24 +56,36 @@ func newClientTrace(logger *log.Logger) *httptrace.ClientTrace {
 		},
 
 		DNSStart: func(info httptrace.DNSStartInfo) {
-			// logger.Printf("[DNSStart] dnsStartInfo: %#v", info)
+			logger.Printf("[DNSStart] Host: %s", info.Host)
 		},
 
 		DNSDone: func(info httptrace.DNSDoneInfo) {
-			//logger.Printf("[DNSDone] dnsDoneInfo: %#v", info)
+			addrs := make([]string, 0, len(info.Addrs))
+			for _, addr := range info.Addrs {
+				addrs = append(addrs, addr.String())
+			}
+			logger.Printf("[DNSDone] Addrs: %s", strings.Join(addrs, ","))
+			logger.Printf("[DNSDone] Coalesced: %v", info.Coalesced)
+			if info.Err != nil {
+				logger.Printf("[DNSDone] Error: %s", info.Err)
+				return
+			}
+			logger.Printf("[DNSDone] Error: nil")
 		},
 
 		ConnectStart: func(network, addr string) {
-			logger.Printf("[ConnectStart] network: %s", network)
-			logger.Printf("[ConnectStart] addr: %s", addr)
+			logger.Printf("[ConnectStart] Network: %s", network)
+			logger.Printf("[ConnectStart] Addr: %s", addr)
 		},
 
 		ConnectDone: func(network, addr string, err error) {
-			logger.Printf("[ConnectDone] network: %s", network)
-			logger.Printf("[ConnectDone] addr: %s", addr)
+			logger.Printf("[ConnectDone] Network: %s", network)
+			logger.Printf("[ConnectDone] Addr: %s", addr)
 			if err != nil {
-				logger.Printf("[ConnectDone] err: %s", err)
+				logger.Printf("[ConnectDone] Error: %s", err)
+				return
 			}
+			logger.Printf("[ConnectDone] Error: nil")
 		},
 
 		WroteHeaders: func() {
@@ -70,10 +97,11 @@ func newClientTrace(logger *log.Logger) *httptrace.ClientTrace {
 		},
 
 		WroteRequest: func(info httptrace.WroteRequestInfo) {
-			logger.Printf("[WroteRequest]")
 			if info.Err != nil {
-				logger.Printf("[WroteRequest] err: %s", info.Err)
+				logger.Printf("[WroteRequest] Error: %s", info.Err)
+				return
 			}
+			logger.Printf("[WroteRequest] Error: nil")
 		},
 	}
 }
